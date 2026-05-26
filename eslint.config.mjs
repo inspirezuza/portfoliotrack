fc9dc032d5727614f2fe0d0f02f9a8516534757c
inspirezuza <inspire7220@gmail.com>
2026-05-27 04:47:28 +0700

    chore: add prettier tooling

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,21 @@
 import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname
+  baseDirectory: __dirname,
 });
 
 const config = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    ignores: [".next/**", "next-env.d.ts", "node_modules/**"]
-  }
+    ignores: [".next/**", "next-env.d.ts", "node_modules/**"],
+  },
+  eslintConfigPrettier,
 ];
 
 export default config;
